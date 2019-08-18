4e61bf7597610711f6c6aa3cdd0d690500ce050a
Reed Molbak <reed@outlook.com>
2019-08-18 09:35:44 -0700

    Add configs and db

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+
+
+class Config(object):
+    ENV = None
+    DEBUG = False
+    SECRET_KEY = os.environ['SECRET_KEY']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    TESTING = False
+
+
+class ProductionConfig(Config):
+    ENV = 'production'
+
+
+class StageConfig(Config):
+    DEBUG = True
+    ENV = 'stage'
+    TESTING = True
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    ENV = 'development'
+    TESTING = True
